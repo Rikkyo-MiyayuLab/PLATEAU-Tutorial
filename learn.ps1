3164cfc8642b2b2f4adcec3a6fa38b3ee54a57c1
tsyu1234 <syukunt@gmail.com>
2025-01-23 10:03:33 +0900

    訓練中にタイムアウトする問題の修正 #19

--- learn.ps1
+++ learn.ps1
@@ -11,14 +11,14 @@ $exePath = "PLATEAUTutorial/build/PLATEAUTutorial.exe"
 
 if($exe) {
     if($force) {
-        mlagents-learn $configurePath --env=$exePath --num-envs=$envs --run-id=$modelId --force --width=1920 --height=1080 --torch-device="cuda"
+        mlagents-learn $configurePath --env=$exePath --num-envs=$envs --run-id=$modelId --force --width=1920 --height=1080 --torch-device="cuda" --timeout=1800
     } else {
-        mlagents-learn $configurePath --env=$exePath --run-id=$modelId --num-envs=$envs --width=1920 --height=1080 --torch-device="cuda" --resume
+        mlagents-learn $configurePath --env=$exePath --run-id=$modelId --num-envs=$envs --width=1920 --height=1080 --torch-device="cuda" --resume --timeout=1800
     }
 } else {
     if($force) {
-        mlagents-learn $configurePath --run-id=$modelId --force --width=1920 --height=1080 --torch-device="cuda"
+        mlagents-learn $configurePath --run-id=$modelId --force --width=1920 --height=1080 --torch-device="cuda" --timeout=1800
     } else {
-        mlagents-learn $configurePath --run-id=$modelId --width=1920 --height=1080 --torch-device="cuda" --resume
+        mlagents-learn $configurePath --run-id=$modelId --width=1920 --height=1080 --torch-device="cuda" --resume --timeout=1800
     }
 }
