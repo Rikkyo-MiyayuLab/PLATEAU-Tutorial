f51dbc2de420e2826c0049be8be2b3f73a2e06ed
tsyu1234 <syukunt@gmail.com>
2025-01-24 17:41:02 +0900

    ハイパラ検証モデル４パターン追加

--- learn.ps1
+++ learn.ps1
@@ -3,22 +3,23 @@ param(
     [int]$mode=1,
     [string]$modelId="0",
     [bool]$force = $false,
-    [bool]$exe = $false
+    [bool]$exe = $false,
+    [int]$port=5005
 )
 
-$configurePath = "PLATEAUTutorial/Assets/Config/Tutorial-$mode.yaml"
+$configurePath = "PLATEAUTutorial/Assets/Config/Tutorial-$mode-param3.yaml"
 $exePath = "PLATEAUTutorial/build/PLATEAUTutorial.exe"
 
 if($exe) {
     if($force) {
-        mlagents-learn $configurePath --env=$exePath --num-envs=$envs --run-id=$modelId --force --width=1920 --height=1080 --torch-device="cuda" --timeout=1800
+        mlagents-learn $configurePath --env=$exePath --num-envs=$envs --run-id=$modelId --force --width=1920 --height=1080 --torch-device="cuda" --timeout=1800 --base-port=$port
     } else {
-        mlagents-learn $configurePath --env=$exePath --run-id=$modelId --num-envs=$envs --width=1920 --height=1080 --torch-device="cuda" --resume --timeout=1800
+        mlagents-learn $configurePath --env=$exePath --run-id=$modelId --num-envs=$envs --width=1920 --height=1080 --torch-device="cuda" --resume --timeout=1800 --base-port=$port
     }
 } else {
     if($force) {
-        mlagents-learn $configurePath --run-id=$modelId --force --width=1920 --height=1080 --torch-device="cuda" --timeout=1800
+        mlagents-learn $configurePath --run-id=$modelId --force --width=1920 --height=1080 --torch-device="cuda" --timeout=1800 --base-port=$port
     } else {
-        mlagents-learn $configurePath --run-id=$modelId --width=1920 --height=1080 --torch-device="cuda" --resume --timeout=1800
+        mlagents-learn $configurePath --run-id=$modelId --width=1920 --height=1080 --torch-device="cuda" --resume --timeout=1800 --base-port=$port
     }
 }
